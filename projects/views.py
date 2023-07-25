2246d213167acd8689ff0af2be525f33d136b6de
emily yim <emilyyim@Emilys-MacBook-Air.local>
2023-07-25 13:35:30 -0700

    Feature 14 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 
 # Create your views here.
@@ -21,3 +22,18 @@ def show_project(request, id):
         "project": get_object_or_404(Project, id=id)
     }
     return render(request, "projects/project_detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    context = {
+        "form": form
+    }
+    return render(request, "projects/create_project.html", context)
