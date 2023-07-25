486686a8541f56708a1b3d2c094d82a9eae4385c
emily yim <emilyyim@Emilys-MacBook-Air.local>
2023-07-25 12:49:54 -0700

    Feature 13 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 
@@ -13,3 +13,11 @@ def list_projects(request):
         "projects": projects
     }
     return render(request, "projects/list_projects.html", context)
+
+
+@login_required
+def show_project(request, id):
+    context = {
+        "project": get_object_or_404(Project, id=id)
+    }
+    return render(request, "projects/project_detail.html", context)
